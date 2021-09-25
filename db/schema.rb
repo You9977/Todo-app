4d77f41413f3c6845b67907a99756acbb2c9c8ce
You9977 <kaounakh0205@yahoo.co.jp>
2021-09-25 14:01:11 +0900

    migrateファイルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_032745) do
+ActiveRecord::Schema.define(version: 2021_09_25_044935) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
     t.datetime "deadline"
+    t.string "title"
   end
 
 end
